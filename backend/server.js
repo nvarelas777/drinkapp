b40bcaac7242259b10cd9e8c0f0b5b5d40ad5079
supbrosef <31687194+supbrosef@users.noreply.github.com>
2019-07-22 20:40:52 -0700

    Changes

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
 const jwt = require('jsonwebtoken');
 const cookieParser = require('cookie-parser');
 require('dotenv').config();
